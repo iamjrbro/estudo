d27153398d34f94aa5805d4866951aa22e526931
Julia Ribeiro <jrstudin@gmail.com>
2023-08-29 18:50:40 -0300

    Create n.js

---
+++ JS/numeros/n.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 5;
+let soma = a + b; // soma agora contém o valor 15
+let produto = a * b; // produto agora contém o valor 50
+
+
+let num = 3.14159;
+let numFormatado = num.toFixed(2)
+
+var c = 0
+var d = 0
+console.log(c/d) // NaN
+
+
+var e = 10
+var f = 0
+console.log(e/f) // Infinity
