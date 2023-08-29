12cc858742f86be72dd96644333b501ffffd1708
Julia Ribeiro <jrstudin@gmail.com>
2023-08-29 18:50:44 -0300

    Create strings.js

---
+++ JS/string/strings.js
@@ -0,0 +1,9 @@
+let nome = 'João'; //aspas simples
+let sobrenome = "Silva"; //aspas duplas
+let mensagem = `Olá, ${nome} ${sobrenome}!`; //template string
+
+
+//concatenação
+let saudacao = 'Olá, ';
+let nome = 'Maria';
+let mensagem = saudacao + nome; //    
