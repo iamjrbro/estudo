ff9c929c0c34531c8fc1d89d7343ffc03644d4fc
Julia Ribeiro <jrstudin@gmail.com>
2023-08-29 18:50:34 -0300

    Create arrowFunction.js

---
+++ JS/funcoes/arrowFunction.js
@@ -0,0 +1,14 @@
+const arrowFunction = nome => `essa função se chama ${nome}` 
+
+const sub = (n1,n1) => n1-n2 //dois parametros
+
+
+//ARROW FUNCTION +1 LINHA DE CODIGO: usar () e {} e codar de acordo com sua condicional
+
+const somaNumerosPequenos = (n1,n2) =>{
+    if(n1>10||n2>10){
+        return 'somente numeros de 1 a 9'
+    } else{
+        return n1+n2
+    }
+}
