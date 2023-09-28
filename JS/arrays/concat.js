f72b46259655a88abef732d571f828af0bdc584f
Julia Ribeiro <jrstudin@gmail.com>
2023-09-28 06:32:19 -0300

    Create concat.js

---
+++ JS/arrays/concat.js
@@ -0,0 +1,6 @@
+//concat: concatena dois arrays, mas não altera nenhum deles. Ele retornará uma nova lista
+
+const salaJS = ['Sara', 'Thiago', 'Pedro', 'Camille']
+const salaPy = ['Ju', 'Leo', 'Carlos', 'Dandara']
+sala = salaJS.concat(salaPy)
+console.log(sala)
