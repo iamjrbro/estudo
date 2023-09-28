d1338ad0698cd5c34d161e29725826550338758f
Julia Ribeiro <jrstudin@gmail.com>
2023-09-28 06:32:33 -0300

    Create lenght.js

---
+++ JS/arrays/lenght.js
@@ -0,0 +1,9 @@
+//Length: atibuto que especifica o tamanho de um array/o numero de elementos/dados contidos no mesmo
+
+const l = [6,8,7,6]
+console.log( l.length)
+
+
+const notas = [10, 8, 6, 2]
+const media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length  
+console.log(media) //dividindo pelo tamanho do array 
