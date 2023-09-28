2822d1bff06ae725606a5bf4c6faaf3b3a4530a5
Julia Ribeiro <jrstudin@gmail.com>
2023-09-28 06:32:46 -0300

    Create split.js

---
+++ JS/arrays/split.js
@@ -0,0 +1,4 @@
+//split: divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array
+const cidade = 'Belo Horizonte'
+const cidades = cidade.split (' ')
+console.log(cidades)
