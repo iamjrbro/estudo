bae2dbc5db247bf3742773bcb7569317e10a6c38
Julia Ribeiro <jrstudin@gmail.com>
2023-09-28 06:32:37 -0300

    Create push.js

---
+++ JS/arrays/push.js
@@ -0,0 +1,11 @@
+//Push: adiciona um item ao final da lista, permitindo a alteração do array
+
+const p = ['Julia']
+p.push('Ribeiro')
+console.log(p)
+
+
+const n = [10, 6, 8];
+n.push(7);
+const me = (n[0] + n[1] + n[2]) / n.length;
+console.log(me);
